d6923801c484016d678948495adfa88027f4f315
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-25 20:32:32 +0300

    Second

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ for(i = 1; i <= 50; i++) {
     console.log(i);
 }
 console.log('Part 2');
-for(i = 35; i >=8; i--) {
+for(i = 35; i >= 8; i--) {
     console.log(i);
+}
+
+// Second
+console.log('Second');
+let k = 89;
+while (k >= 11) {
+    console.log(k);
+    document.write(k, '<br/>');
+    k = k - 1;
 }
