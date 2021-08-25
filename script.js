62492d9993f56309162625535b52711916fa05e7
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-25 20:21:01 +0300

    First

---
+++ script.js
@@ -0,0 +1,10 @@
+//First 
+console.log('First');
+console.log('Part 1');
+for(i = 1; i <= 50; i++) {
+    console.log(i);
+}
+console.log('Part 2');
+for(i = 35; i >=8; i--) {
+    console.log(i);
+}
